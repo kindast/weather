52edbe1653f81cb5182cdf94b05f49cfb6611d69
kindast <61793652+kindast@users.noreply.github.com>
2020-04-24 17:28:26 +0300

    Added config file

--- GUI Version/EN/Weather.py
+++ GUI Version/EN/Weather.py
@@ -6,7 +6,7 @@ from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
 from PySide2.QtWidgets import *
 from MainUI import Ui_MainWindow
 from WeatherUI import Ui_WeatherWindow
-import sys
+import sys, config
 import requests
 
 app = QApplication(sys.argv)
@@ -26,28 +26,41 @@ def get_city():
 def get_coords(city):
     res = requests.get("https://geocode-maps.yandex.ru/1.x",
                        params={"geocode": city,
-                               "apikey": "db527ae8-6405-44df-91da-5cec4d049af6",
+                               "apikey": config.GeocoderAPIKey,
                                "sco": "latlong",
                                "format": "json",
                                "results": "1",
                                "lang": "en_RU"}
                        )
-    data = res.json()
-    coords = ((((((data["response"])["GeoObjectCollection"])["featureMember"])[0])["GeoObject"])["Point"])["pos"].partition(" ")
-    coords = {"lat": coords[2], "lon": coords[0]}
-    return coords
+    if not res.status_code == 200:
+      error("Wrong API key for Yandex.Geocoder")
+    else:
+      data = res.json()
+      coords = ((((((data["response"])["GeoObjectCollection"])["featureMember"])[0])["GeoObject"])["Point"])["pos"].partition(" ")
+      coords = {"lat": coords[2], "lon": coords[0]}
+      return coords
 
 def get_weather(lat, lon):
-    YandexAPIkey = "0c0ec0c2-2aeb-4710-9e72-be9f4127ad6f"
     res = requests.get("https://api.weather.yandex.ru/v1/forecast/",
                        params={"lat": lat, "lon": lon,
                                "lang": "en_US", "limit": 1},
-                       headers={"X-Yandex-API-Key": YandexAPIkey}
+                       headers={"X-Yandex-API-Key": config.WeatherAPIKey}
                        )
-    data = res.json()
-    weather = data["fact"]
-    return weather
+    if not res.status_code == 200:
+      error("Wrong API key for Yandex.Weathers")
+    else:
+      data = res.json()
+      return data["fact"]
 
+def error(text):
+    mbox = QMessageBox()
+    mbox.setWindowTitle("Error")
+    mbox.setText(text)
+    icon = QIcon()
+    icon.addFile(u"icons/error.png", QSize(), QIcon.Normal, QIcon.Off)
+    mbox.setWindowIcon(icon)
+    mbox.show()
+    mbox.exec()
 
 def auto_city(self):
     city = get_city()
@@ -76,14 +89,7 @@ def weather(self):
         WeatherWindow.show()
         WeatherWindow.exec()
     except IndexError:
-        mbox = QMessageBox()
-        mbox.setWindowTitle("Error")
-        mbox.setText("The city was entered incorrectly or there is no Internet connection")
-        icon = QIcon()
-        icon.addFile(u"icons/error.png", QSize(), QIcon.Normal, QIcon.Off)
-        mbox.setWindowIcon(icon)
-        mbox.show()
-        mbox.exec()
+      error("The city was entered incorrectly or there is no Internet connection")
 
 ui.wbtn.clicked.connect(auto_city)
 ui.wbtnr.clicked.connect(weather)
--- GUI Version/RU/Weather.py
+++ GUI Version/RU/Weather.py
@@ -6,7 +6,7 @@ from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
 from PySide2.QtWidgets import *
 from MainUI import Ui_MainWindow
 from WeatherUI import Ui_WeatherWindow
-import sys
+import sys, config
 import requests
 
 app = QApplication(sys.argv)
@@ -16,7 +16,6 @@ ui = Ui_MainWindow()
 ui.setupUi(MainWindow)
 MainWindow.show()
 
-
 def get_city():
     res = requests.get("http://ip-api.com/json?fields=country,city",
                        params={"lang": "ru"}
@@ -24,62 +23,72 @@ def get_city():
     data = res.json()
     return data['city']
 
-
 def get_coords(city):
     res = requests.get("https://geocode-maps.yandex.ru/1.x",
                        params={"geocode": city,
-                               "apikey": "db527ae8-6405-44df-91da-5cec4d049af6",
+                               "apikey": config.GeocoderAPIKey,
                                "sco": "latlong",
                                "format": "json",
                                "results": "1",
                                "lang": "ru_RU"}
                        )
-    data = res.json()
-    coords = ((((((data["response"])["GeoObjectCollection"])["featureMember"])[0])[
-              "GeoObject"])["Point"])["pos"].partition(" ")
-    coords = {"lat": coords[2], "lon": coords[0]}
-    return coords
-
+    if not res.status_code == 200:
+      error("Неправильный API ключ Яндекс.Геокодера")
+    else:
+      data = res.json()
+      coords = ((((((data["response"])["GeoObjectCollection"])["featureMember"])[0])["GeoObject"])["Point"])["pos"].partition(" ")
+      coords = {"lat": coords[2], "lon": coords[0]}
+      return coords
 
 def get_weather(lat, lon):
-    YandexAPIkey = "0c0ec0c2-2aeb-4710-9e72-be9f4127ad6f"
     res = requests.get("https://api.weather.yandex.ru/v1/forecast/",
                        params={"lat": lat, "lon": lon,
                                "lang": "ru_RU", "limit": 1},
-                       headers={"X-Yandex-API-Key": YandexAPIkey}
+                       headers={"X-Yandex-API-Key": config.WeatherAPIKey}
                        )
-    data = res.json()
-    weather = data["fact"]
-    # Traslate
-    if weather["condition"] == "clear":
-        weather["condition"] = "Ясно"
-    elif weather["condition"] == "cloudy":
-        weather["condition"] = "Облачно с прояснениями"
-    elif weather["condition"] == "overcast":
-        weather["condition"] = "Пасмурно"
-    elif weather["condition"] == "partly-cloudy":
-        weather["condition"] = "Малооблачно"
-    elif (weather["condition"] == "partly-cloudy-and-light-rain") or (weather["condition"] == "cloudy-and-light-rain") or (weather["condition"] == "overcast-and-light-rain"):
-        weather["condition"] = "Небольшой дождь"
-    elif (weather["condition"] == "partly-cloudy-and-rain") or (weather["condition"] == "cloudy-and-rain"):
-        weather["condition"] = "Дождь"
-    elif weather["condition"] == "overcast-and-rain":
-        weather["condition"] = "Сильный дождь"
-    elif weather["condition"] == "overcast-thunderstorms-with-rain":
-        weather["condition"] = "Сильный дождь, гроза"
-    elif weather["condition"] == "overcast-and-wet-snow":
-        weather["condition"] = "Дождь со снегом"
-    elif weather["condition"] == "partly-cloudy-and-light-snow":
-        weather["condition"] = "Небольшой снег"
-    elif weather["condition"] == "partly-cloudy-and-snow":
-        weather["condition"] = "Снег"
-    return weather
+    if not res.status_code == 200:
+      error("Неправильный API ключ Яндекс.Погоды")
+    else:
+        data = res.json()
+        weather = data["fact"]
+        #Traslate
+        if weather["condition"] == "clear":
+          weather["condition"] = "Ясно"
+        elif weather["condition"] == "cloudy":
+          weather["condition"] = "Облачно с прояснениями"
+        elif weather["condition"] == "overcast":
+          weather["condition"] = "Пасмурно"
+        elif weather["condition"] == "partly-cloudy":
+          weather["condition"] = "Малооблачно"
+        elif (weather["condition"] == "partly-cloudy-and-light-rain") or (weather["condition"] == "cloudy-and-light-rain") or (weather["condition"] == "overcast-and-light-rain"):
+          weather["condition"] = "Небольшой дождь"
+        elif (weather["condition"] == "partly-cloudy-and-rain") or (weather["condition"] == "cloudy-and-rain"):
+          weather["condition"] = "Дождь"
+        elif weather["condition"] == "overcast-and-rain":
+          weather["condition"] = "Сильный дождь"
+        elif weather["condition"] == "overcast-thunderstorms-with-rain":
+          weather["condition"] = "Сильный дождь, гроза"
+        elif weather["condition"] == "overcast-and-wet-snow":
+          weather["condition"] = "Дождь со снегом"
+        elif weather["condition"] == "partly-cloudy-and-light-snow":
+          weather["condition"] = "Небольшой снег"
+        elif weather["condition"] == "partly-cloudy-and-snow":
+          weather["condition"] = "Снег"
+        return weather
 
+def error(text):
+    mbox = QMessageBox()
+    mbox.setWindowTitle("Ошибка")
+    mbox.setText(text)
+    icon = QIcon()
+    icon.addFile(u"icons/error.png", QSize(), QIcon.Normal, QIcon.Off)
+    mbox.setWindowIcon(icon)
+    mbox.show()
+    mbox.exec()
 
 def auto_city(self):
     city = get_city()
     ui.lineedit.setText(city)
-
 
 def weather(self):
     try:
@@ -104,15 +113,7 @@ def weather(self):
         WeatherWindow.show()
         WeatherWindow.exec()
     except IndexError:
-        mbox = QMessageBox()
-        mbox.setWindowTitle("Ошибка")
-        mbox.setText("Неправильно введён город или нет подключения к Интернету")
-        icon = QIcon()
-        icon.addFile(u"icons/error.png", QSize(), QIcon.Normal, QIcon.Off)
-        mbox.setWindowIcon(icon)
-        mbox.show()
-        mbox.exec()
-
+        error("Неправильно введён город или нет подключения к Интернету")
 
 ui.wbtn.clicked.connect(auto_city)
 ui.wbtnr.clicked.connect(weather)
